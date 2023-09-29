8e4907ea6432a396a2636e24a12390237d6c2f9c
hjasier <asiaurre@gmail.com>
2023-09-29 08:27:11 +0000

    bugfixMemes y Encuestas

--- Duricleto/cogs/encuestas.py
+++ Duricleto/cogs/encuestas.py
@@ -13,6 +13,8 @@ from typing import List
 import os
 import json
 
+
+
 encuestas_activas = {}
 
 ruta = "/home/bot/BotV6/Duricleto/cogs/archivos/encuestas.json"
@@ -23,8 +25,6 @@ async def updateEmbed(ctx):
     embed=discord.Embed(title=f"{encuesta['titulo']}")
 
     total = sum([len(encuesta['respuestas'][vts]) for vts in encuesta['respuestas']])
-    print(f"Total --> {total}")
-    
     for opc in encuesta['opciones']:
         porcentaje = (len(encuesta['respuestas'][encuesta['opciones'].index(opc)])/total)*100
         prtj = "🟪" * round(porcentaje/10)
@@ -32,34 +32,79 @@ async def updateEmbed(ctx):
         embed.add_field(name=f"{opc}", value=f"{prtj}      {porcentaje}%", inline=False)
     embed.set_footer(text=f"Numero de votos {total}")
     await ctx.message.edit(embed=embed)
+    updateLocalFile()
     
+
+async def votar_callback(select, embedmsg, encuestas_activas):
+    t = select.data['values'][0]
+    user_id = select.user.id
+    enc_id = (t.replace(" ",""))[:15]
+    encuesta = encuestas_activas[embedmsg.id]
     
+    opcIndex = encuesta['opciones'].index(t)
     
+    for opc in range(len(encuesta['opciones'])):
+        try:
+            if str(opc) in encuesta['respuestas']:
+                if user_id in encuesta['respuestas'][opc]:
+                    encuesta['respuestas'][str(opc)].remove(user_id)
+            else:
+                encuesta['respuestas'][str(opc)] = []
+        except:
+            pass
+    
+    encuesta['respuestas'][opcIndex].append(user_id)
+    await updateEmbed(select)
+    await select.response.defer()
+    
+def loadEncuestas():
+    global encuestas_activas
+    try:
+        with open(ruta, 'r') as archivo:
+            encuestas_activas = json.load(archivo)
+            encuestas_activas = {int(key): value for key, value in encuestas_activas.items()}
+    except FileNotFoundError:
+        encuestas_activas = {}
+loadEncuestas()
+
+
+def updateLocalFile():
+    global encuestas_activas
+    try:
+        with open(ruta, 'w') as archivo:
+            json.dump(encuestas_activas, archivo, indent=4)
+    except Exception as e:
+        print(f"Error al actualizar el archivo: {str(e)}")
         
 class EncuestaCog(commands.Cog):
     def __init__(self, client):
         self.client = client
         
-
-
-    def loadEncuestas():
-        global encuestas_activas
-        try:
-            with open(ruta, 'r') as archivo:
-                encuestas_activas = json.load(archivo)
-        except FileNotFoundError:
-            encuestas_activas = {}
     
-    
-    
-    def updateLocalFile():
-        global encuestas_activas
-        try:
-            with open(ruta, 'w') as archivo:
-                json.dump(encuestas_activas, archivo, indent=4)
-        except Exception as e:
-            print(f"Error al actualizar el archivo: {str(e)}")
 
+        
+    async def reloadEncuestas(self):
+        for encuesta in encuestas_activas:
+            encuesta = encuestas_activas[encuesta]
+            print("Relodeando encuesta ")
+            channel = self.client.get_channel(int(encuesta['chnnl']))
+            msg = await channel.fetch_message(int(encuesta['_id']))
+            
+            class Selector(discord.ui.View):
+                @discord.ui.select(
+                    placeholder = "📨 Elige tu respuesta 📨",
+                    min_values = 1, 
+                    max_values = 1, 
+                    options = [discord.SelectOption(label=option) for option in encuesta['opciones']]
+                )
+                async def select_callback(self, select, interaction):
+                    await votar_callback(select, msg, encuestas_activas)      
+            await msg.edit(view=Selector())
+            
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.reloadEncuestas()
 
     @commands.hybrid_command(name="encuesta",description="Crea una encuesta")           
     @app_commands.describe(titulo="Titulo de la encuesta")
@@ -78,35 +123,13 @@ class EncuestaCog(commands.Cog):
 
         class Selector(discord.ui.View):
             @discord.ui.select(
-                placeholder = "Envia tu respuesta 📨",
+                placeholder = "Elige tu respuesta 📨",
                 min_values = 1, 
                 max_values = 1, 
                 options = [discord.SelectOption(label=option) for option in opciones]
             )
             async def select_callback(self, select, interaction):
-               t = select.data['values'][0]
-               user_id = select.user.id
-               enc_id = (t.replace(" ",""))[:15]
-               
-
-               encuesta = encuestas_activas[embedmsg.id]
-               
-               opcIndex = encuesta['opciones'].index(t)
-                
-               for opc in range(len(encuesta['opciones'])):
-                    try:
-                        if opc in encuesta['respuestas']:
-                            if user_id in encuesta['respuestas'][opc]:
-                                encuesta['respuestas'][opc].remove(user_id)
-                        else:
-                            encuesta['respuestas'][opc] = []
-                    except:
-                        pass
-               encuesta['respuestas'][opcIndex].append(user_id)
-               await updateEmbed(select)
-                
-               print(encuestas_activas)
-               await select.response.defer()   
+               await votar_callback(select, embedmsg, encuestas_activas)
 
                 
         embedmsg = await ctx.send(embed=embed , view=Selector())
@@ -117,6 +140,7 @@ class EncuestaCog(commands.Cog):
             'opciones':opciones,
             'respuestas':{},
             'author':ctx.author.id,
+            'chnnl':ctx.channel.id
         }
         global encuestas_activas
         encuestas_activas[embedmsg.id] = encuesta
